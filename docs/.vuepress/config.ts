70b4a2aca1ba00f729394a0975430a0d9c4398fb
sikychen <sikychen@gmail.com>
2021-12-05 13:52:50 +0800

    add deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,8 @@ import { defineUserConfig } from '@vuepress/cli'
 import type { DefaultThemeOptions } from '@vuepress/theme-default'
 
 export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
+  base: 'blog_v2',
+  
   lang: 'zh-CN',
   title: '老白的驿站',
   description: '老白涮肉坊欢迎你，钱钱钱，没给钱呢！',
